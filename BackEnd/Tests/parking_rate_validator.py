c799692a102d536b1d0434e4208072472abf72ad
Alan Vo <124543342+ancomalan@users.noreply.github.com>
2025-03-27 17:42:39 -0500

    Deleted my old parking rate tests

--- BackEnd/Tests/parking_rate_validator.py
+++
@@ -1,4 +0,0 @@
-validLotsOnCampus = {"Union_lot", "Asp_lot", "Jenkins_lot", "Elm_lot", "Timberdell_lot"} #using set
-
-def validate_parking_rate(parkingLot):
-    return isinstance(parkingLot, str) and parkingLot.endswith("_lot") and parkingLot in validLotsOnCampus
