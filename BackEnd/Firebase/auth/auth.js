f88c301ba5c098ab8afc270b71078ba9b90506d8
Alan Vo <bvo101968@gmail.com>
2025-04-13 17:20:07 -0500

    Added sign in with google pop up, which also saves email and full name to realtime database

--- BackEnd/Firebase/auth/auth.js
+++ BackEnd/Firebase/auth/auth.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { firebaseConfig } from './config.js'; //ijmport firebase configuration file from config.js
-
+import { firebaseConfig } from './config.js'; //import firebase configuration file from config.js
+import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js"; //import google auth provider and sign in with popup function
 const app = initializeApp(firebaseConfig); //initialize firebase app with configuration file
 
 //get references to real time database and authentication
 const auth = getAuth(app);
 const database = getDatabase(app);
 
+//sign up with email and password 
 let signUpButton = document.getElementById("signUpButton");
 //add event listener to sign up button
 signUpButton.addEventListener("click", function () {
@@ -26,12 +27,37 @@ signUpButton.addEventListener("click", function () {
         //"set" saves data to a specified reference
         set(ref(database, 'users/' + user.uid), {
             email: user.email,
-            displayName: user.displayName
           });
         alert("User signed up succesfully!"); //alert user that they have signed up successfully
-        _})
+        })
         .catch((error) => {
         alert(error.message); //alert user if there is an error signing up
         });
 });
+
+//sign in with google pop up 
+let signInGoogleButton = document.getElementById("signInGoogleButton");
+const provider =  new GoogleAuthProvider(); //create instance of google provider 
+//add event listener to sign in with google button 
+signInGoogleButton.addEventListener("click", function () {
+    //use firebase authentication to sign in with google 
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+
+            //store user information to database
+            set(ref(database, 'users/' + user.uid), {
+                email: user.email,
+                fullName: user.displayName, 
+              });
+            alert("Signed in with Google as " + user.displayName); //alert user that they have signed in successfully
+        }).catch((error) => {
+            alert(error.message); //alert user if there is an error signing in
+        });
+}); 
+
 
