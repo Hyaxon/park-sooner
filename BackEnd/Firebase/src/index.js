e53d15773ec0c3be719259e3b61e1e14394f3839
Alan Vo <bvo101968@gmail.com>
2025-04-15 13:18:13 -0500

    Updated parking write function

--- BackEnd/Firebase/src/index.js
+++
@@ -1,5 +0,0 @@
-import { initializeApp  } from "firebase/app";
-import { getrealtime } from "firebase/database";
-import { getAuth } from "firebase/auth";
-
-// const firebaseApp = initializeApp({
--- BackEnd/Firebase/src/script.js
+++ BackEnd/Firebase/src/script.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 import { firebaseConfig } from './config.js';
@@ -8,27 +7,33 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 let button = document.getElementById("button");
-
 button.addEventListener("click", function () {
   //get values from input fields
   let lotName = document.getElementById("lotName").value;
   let lotCapacity = document.getElementById("lotCapacity").value;
+  let hourlyRate = document.getElementById("hourlyRate").value;
 
-  //print error if input fields are empty
-  if (lotName === "" || lotCapacity === "") {
-    alert("Please enter all fields.");
-  } 
-  else {
-    //make object to store in database
-    const parkingData = {
-      lotName: lotName,
-      lotCapacity: lotCapacity
-    };
+  //get selected permit types and if there are pay stations or not
+  let selectedPermits = [];
+  document.querySelectorAll('input[name="permit"]:checked').forEach((checkbox) => {
+    selectedPermits.push(checkbox.value);
+  });
 
-    //save data to firebase realtime database    
-    const reference = ref(database, "parkingData"); //create reference to the database
+  //create object to store parking lot information in database
+  const parkingData = {
+    lotName: lotName,
+    lotCapacity: lotCapacity,
+    permitTypes: selectedPermits
+  };
 
-    //push object to reference parkingData
-    push(reference, parkingData);
+  // If hourly rate is provided, add it
+  if (hourlyRate.trim() !== "") {
+    parkingData.hourlyRate = parseFloat(hourlyRate);
   }
-});
+  //save data to firebase realtime database    
+  const reference = ref(database, "parkingData"); //create reference to the database
+
+  //push object to reference parkingData
+  push(reference, parkingData);
+}
+);
