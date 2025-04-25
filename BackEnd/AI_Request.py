0caf431bae8ca214a2ab48131cfbca5bcc592f92
stupe <stupefied17@gmail.com>
2025-04-25 15:55:09 -0500

    Merge branch 'main' of https://github.com/mitchellmeasley/Park-Sooner

---
+++ BackEnd/AI_Request.py
@@ -0,0 +1,44 @@
+import time
+import os
+from dotenv import load_dotenv, dotenv_values
+import datetime
+import openai
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(api_key = "OPENAI_API_KEY")
+
+def throttle(interval):
+    def decorator(func):
+        last_called = 0
+        def wrapper(*args, **kwargs):
+            nonlocal last_called
+            now = time.time()
+            if now - last_called >= interval:
+                last_called = now
+                return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+#@throttle(120)
+def system_request():
+
+    messages = [{"role": "system", "content": "You make parking predictions based on current parking data."}]
+
+    while True:
+        message = input("User : ")
+        if message:
+            messages.append({"role": "user", "content": message})
+            response = openai.chat.completions.create(
+                model="gpt-4.1-nano",
+                messages=messages,
+                temperature=0.3,
+                max_tokens=50
+            )
+            reply = response.choices[0].message.content
+            print("Parker : ", reply)
+            messages.append({"role": "system", "content": reply})
+
+
+system_request()
